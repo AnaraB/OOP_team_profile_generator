4acb0b9d8ac6a8d063340db44f56f4fb9e9e6cf5
Anara <berdali.aygerim9@gmail.com>
2024-02-24 17:56:49 +0000

    engineer and intern propmpts added

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
+const { start } = require("repl");
 
 let teamMembers = [];
 
@@ -17,12 +18,14 @@ const teamManagerInfo= [
   {
     type: "input",
     name: "teamManagerName",
-    message: "Enter your name"
+    message: "Enter your name",
+    validate: val => /[a-z]/gi.test(val)       
   },
   {
     type: "input",
     name: "employeeID",
-    message: "Enter your employee ID"
+    message: "Enter your employee ID",
+    validate: val => /[1-9]/gi.test(val)        
   },
   {
     type: "input",
@@ -32,22 +35,73 @@ const teamManagerInfo= [
   {
     type: "input",
     name: "officeNumber",
-    message: "Enter your office number"
+    message: "Enter your office number",
+    validate: val => /[1-9]/gi.test(val)        
   }
 
 ]
-// add validation to ensure that user input is in the proper format.
-//use inquirer 
+
+  const engineerInfo = [
+    {
+      type: "input",
+      name: "engineerName",
+      message: "Enter engineer name",
+      validate: val => /[a-z]/gi.test(val)   
+    },
+    {
+      type: "input",
+      name: "engineerID",
+      message: "Enter ID number",
+      validate: val => /[1-9]/gi.test(val)     
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter email address"
+    },
+    {
+      type: "input",
+      name: "gitHubUsername",
+      message: "Enter gitHub username"
+    }
+  ]
+
+  const internInfo = [
+    {
+      type: "input",
+      name: "internName",
+      message: "Enter intern name",
+      validate: val => /[a-z]/gi.test(val)   
+    },
+    {
+      type: "input",
+      name: "internID",
+      message: "Enter ID number",
+      validate: val => /[1-9]/gi.test(val)     
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter email address"
+    },
+    {
+      type: "input",
+      name: "school",
+      message: "Enter school"
+    }
+
+  ]
+
+// BONUS task - add validation to ensure that user input is in the proper format.
+//use inquirer  to prompt manager info
 inquirer.prompt( teamManagerInfo).then(answers =>{
   console.log(answers);
+  //create instance obj of Manager class
   const newManagerAnswers = new Manager(answers.teamManagerName, answers.employeeID, answers.email, answers.officeNumber);
   console.log(newManagerAnswers);
   teamMembers.push(newManagerAnswers);
   startMenu();
-
-
 });
-
 
 function startMenu(){
   inquirer.prompt([
@@ -58,25 +112,44 @@ function startMenu(){
       name: 'menuOptions'
     }
   ])
+  .then(val => {
+    if(val.menuOptions === 'Add an engineer'){
+      inquirer.prompt(engineerInfo).then(answers => {
+          //create instance obj of Engineer class
+       const newEngineerAnswers = new Engineer(answers.engineerName, answers.employeeID, answers.email, answers.gitHubUsername);
+       console.log(newEngineerAnswers)
+       teamMembers.push(newEngineerAnswers);
+       console.log(teamMembers);
+       startMenu()
+      })
+    } else if (val.menuOptions === 'Add an intern'){
+      inquirer.prompt(internInfo).then(answers => {
+        //create instance obj of Engineer class
+     const newInternAnswers = new Intern(answers.internName, answers.internID, answers.email, answers.school);
+     console.log(newInternAnswers)
+     teamMembers.push(newInternAnswers);
+     console.log(teamMembers);
+     startMenu();
+    })
+    } else {
+      finishBuildingTheTeam()
+    }
+  })
 
 }
 
-function addEngineer(){
-
-}
-
-function addIntern(){
-
-}
 
 function finishBuildingTheTeam(){
+  //exit application and html is generated 
 
-}
-
-
-
-// fs.appendFile(outputPath, render, function (err) {
+  // fs.appendFile(outputPath, render, function (err) {
 //   if (err) throw err;
 //   console.log('Saved!');
 // });
+
+
+
+}
+
+
 
