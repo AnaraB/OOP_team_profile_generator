fa6be27cfa4f89778304e8db998bf533c8decdb9
Anara <berdali.aygerim9@gmail.com>
2024-02-24 14:58:25 +0000

    teamManagerQuestions array added and prompt created

--- index.js
+++ index.js
@@ -12,7 +12,40 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+const teamManagerQuestions= [
+  {
+    type: "input",
+    name: "teamManagerName",
+    message: "Enter your name"
+  },
+  {
+    type: "input",
+    name: "employeeID",
+    message: "Enter your employee ID"
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email address"
+  },
+  {
+    type: "input",
+    name: "office number",
+    message: "Enter your office number"
+  }
 
-
+]
+// add validation to ensure that user input is in the proper format.
 //use inquirer 
+inquirer.prompt( teamManagerQuestions).then(answer =>{
 
+  let data = createData(answer);
+  fs.writeFileSync(`${answer}`, data)
+});
+
+
+
+// after that the user is presented with a menu with the option to:
+// Add an engineer
+// Add an intern
+// Finish building the team
